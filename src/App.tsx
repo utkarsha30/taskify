d99e95ce00af60cc0eb649617dd7671caa0c0e59
Utkarsha Kshirsagar <utkarshakshirsagar4717@gmail.com>
2025-11-20 08:48:52 -0500

    added webawesome css for title

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App: React.FC = () => {
     <div className="App">
       {/* Add text color to be white */}
 
-      <span className="wa-stack wa-align-items-center  wa-font-size-3xl wa-font-weight-bold ">
+      <span className="wa-stack wa-align-items-center wa-font-size-3xl wa-font-weight-bold " >
         Taskify
       </span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
